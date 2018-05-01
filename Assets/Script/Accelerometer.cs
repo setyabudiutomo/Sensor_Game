5f6539bac95d6247e2545972e8f5f4bceb0d759d
DESKTOP-RJPVIMO\BUDI <budiutomo070@gmail.com>
2018-05-01 18:38:41 +0700

    Penghilangan beberapa script yang tidak terpakai

--- Assets/Script/Accelerometer.cs
+++ Assets/Script/Accelerometer.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Accelerometer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(-Input.acceleration.x * 0.1f, 0,Input.acceleration.z * 0.1f);
